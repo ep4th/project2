4b59020dfc70aa36e026b56d07e97dfa901b5a4f
Guillermo Garcia Jr <guillermo.jr23@outlook.com>
2019-04-06 11:41:24 -0700

    Map is now rendering.

--- public/js/map.js
+++ public/js/map.js
@@ -1,67 +1,14 @@
-var customLabel = {
-    coder: {
-      label: 'C'
-    }
+function myMap() {
+  var mapProp= {
+    center:new google.maps.LatLng(34.0522,-118.2437),
+    zoom:12,
   };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-    function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 34.0522, lng: -118.2437},
-        zoom: 8
-        });
-    var infoWindow = new google.maps.InfoWindow;
-
-      // Change this depending on the name of your PHP or XML file
-      downloadUrl('https://github.com/ep4th/project2/blob/irene/power/.php', function(data) {
-        var xml = data.responseXML;
-        var markers = xml.documentElement.getElementsByTagName('marker');
-        Array.prototype.forEach.call(markers, function(markerElem) {
-          var id = markerElem.getAttribute('id');
-          var name = markerElem.getAttribute('name');
-          var zip = markerElem.getAttribute('zip');
-          var point = new google.maps.LatLng(
-              parseFloat(markerElem.getAttribute('lat')),
-              parseFloat(markerElem.getAttribute('lng')));
-
-          var infowincontent = document.createElement('div');
-          var strong = document.createElement('strong');
-          strong.textContent = name
-          infowincontent.appendChild(strong);
-          infowincontent.appendChild(document.createElement('br'));
-
-          var text = document.createElement('text');
-          text.textContent = zip
-          infowincontent.appendChild(text);
-          var icon = customLabel[type] || {};
-          var marker = new google.maps.Marker({
-            map: map,
-            position: point,
-            label: icon.label
-          });
-          marker.addListener('click', function() {
-            infoWindow.setContent(infowincontent);
-            infoWindow.open(map, marker);
-          });
-        });
-      });
-    }
-
-
-
-  function downloadUrl(url, callback) {
-    var request = window.ActiveXObject ?
-        new ActiveXObject('Microsoft.XMLHTTP') :
-        new XMLHttpRequest;
-
-    request.onreadystatechange = function() {
-      if (request.readyState == 4) {
-        request.onreadystatechange = doNothing;
-        callback(request, request.status);
-      }
-    };
-
-    request.open('GET', url, true);
-    request.send(null);
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
   }
-
-  function doNothing() {}
+  myMap();
