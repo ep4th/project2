057c5e00010c1b899ff5f1784340f12a20ca052b
Robert Power <epower4th@hotmail.com>
2019-04-06 11:15:11 -0700

    power5

--- views/js/simple.js
+++ views/js/simple.js
@@ -1,10 +1,6 @@
 var map;
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 34.0522, lng: -118.2437},
-      zoom: 8
-      });
-      infowindow = new google.maps.InfoWindow({
-        content: document.getElementById('form')
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 34.0522, lng: -118.2437},
+          zoom: 8
         });
-      }
